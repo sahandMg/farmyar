65e467c674925720ebf207ca46bc6954e0a5e665
sahandMg <sahand.mg.ne@gmail.com>
2019-09-12 23:27:03 +0430

    dashboard image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,9 +17,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-//       if(config('app.env') === 'production') {
-//           URL::forceScheme('https');
-//       }
+       if(config('app.env') === 'production') {
+           URL::forceScheme('https');
+       }
     }
 
     /**
